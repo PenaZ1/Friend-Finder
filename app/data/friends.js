86c50e0735cd9aeeb4bef9813fd183ef512b4e60
Zachary <Omitakura@gmail.com>
2019-12-08 22:47:22 -0600

    added in html for home page and survey page with basic server js and routing

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name: "Boruto Uzumaki",
+        photo: "https://vignette.wikia.nocookie.net/naruto/images/b/bd/Boruto_uzumaki.png/revision/latest/scale-to-width-down/300?cb=20150416123755",
+        scores: [5, 2, 5, 1, 1, 1, 5, 2, 5, 1],
+    },
+    {
+        name: "Sarada Uchiha",
+        photo: "https://vignette.wikia.nocookie.net/naruto/images/9/9a/Sarada_Infobox.png/revision/latest/scale-to-width-down/300?cb=20180531174206",
+        scores: [2, 3, 1, 4, 4, 5, 2, 3, 4, 5],
+    },
+    {
+        name: "Metal Lee",
+        photo: "https://vignette.wikia.nocookie.net/naruto/images/9/93/Metal_Lee.png/revision/latest/scale-to-width-down/300?cb=20180824114015",
+        scores: [3, 1, 3, 3, 3, 4, 3, 5, 1, 4],
+    },
+    {
+        name: "Denki",
+        photo: "https://vignette.wikia.nocookie.net/naruto/images/a/a3/Denki_Kaminarimon.png/revision/latest/scale-to-width-down/300?cb=20180208013944",
+        scores: [2, 5, 4, 5, 5, 4, 2, 4, 2, 3],
+    },
+    {
+        name: "Shikadai Nara",
+        photo: "https://vignette.wikia.nocookie.net/naruto/images/3/3d/Shikadai_Nara.png/revision/latest?cb=20150624043352",
+        scores: [1, 4, 2, 5, 5, 3, 1, 1, 3, 2],
+    },
+    {
+        name: "Inojin Yamanaka",
+        photo: "https://vignette.wikia.nocookie.net/naruto/images/e/e6/Inojin_movie.png/revision/latest?cb=20160715224617",
+        scores: [4, 3, 3, 2, 2, 3, 4, 3, 4, 3],
+    },
+];
+
+module.exports = friends;
